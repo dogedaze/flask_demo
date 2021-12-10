6d11feb6ac0ed917f8ff2448ba6593924e79ed97
dogedaze <95709513+dogedaze@users.noreply.github.com>
2021-12-10 11:55:15 -0500

    changes 1

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_bootstrap import Bootstrap
+import pymongo
+import json
+import requests
+from modules import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+client = pymongo.MongoClient("mongodb://pythonforfinance:mypassword@cluster0-shard-00-00-apl4w.mongodb.net:27017,cluster0-shard-00-01-apl4w.mongodb.net:27017,cluster0-shard-00-02-apl4w.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client['historical_prices']
 migrate = Migrate(app, db)
 bootstrap=Bootstrap(app)
 from app import routes, models
--- app/modules.py
+++ app/modules.py
@@ -17,3 +17,19 @@ def current_ticker_price():
     ticker_open = ticker_data.filter(['Open'])
     ticker_open = ticker_open.rename(columns = {"Open": ticker_name}) 
     print(ticker_open)
+
+
+import pymongo
+import json
+import requests
+#retrieving historical prices from Apple.
+historical_prices = requests.get('https://financialmodelingprep.com/api/v3/historical-chart/1min/AAPL?apikey=9cd6b96b4b472eb298b9178e91959c51')
+
+#retrieving historical prices from Apple.
+historical_prices = requests.get('https://financialmodelingprep.com/api/v3/historical-chart/1min/AAPL?apikey=9cd6b96b4b472eb298b9178e91959c51')
+
+client = pymongo.MongoClient("mongodb://pythonforfinance:mypassword@cluster0-shard-00-00-apl4w.mongodb.net:27017,cluster0-shard-00-01-apl4w.mongodb.net:27017,cluster0-shard-00-02-apl4w.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client['historical_prices']
+print (db)
+earnings_surprises = requests.get ('https://financialmodelingprep.com/api/v3/earnings-surprises/AAPL?apikey=9cd6b96b4b472eb298b9178e91959c51')
+earnings_surprises = earnings_surprises.json()
